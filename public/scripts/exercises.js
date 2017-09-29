be415621bce330484690987396bf43a74f9564b5
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-09-29 12:55:01 -0500

    continued styling the workout view

--- public/scripts/exercises.js
+++ public/scripts/exercises.js
@@ -3,79 +3,79 @@
 const exercises = [{
   id: 0, // int
   name: 'Bicycle Crunches', // str
-  gifUrl: 'assets/bicycle_crunches.gif'// str
+  gifUrl: 'assets/workouts/bicycle_crunches.gif'// str
 }, {
   id: 1, // int
   name: 'Burpees', // str
-  gifUrl: 'assets/burpees.gif'// str
+  gifUrl: 'assets/workouts/burpees.gif'// str
 }, {
   id: 2, // int
   name: 'Butt Kicks', // str
-  gifUrl: 'assets/butt_kicks.gif'// str
+  gifUrl: 'assets/workouts/butt_kicks.gif'// str
 }, {
   id: 3, // int
   name: 'Frog Jumps', // str
-  gifUrl: 'assets/frog_jumps.gif'// str
+  gifUrl: 'assets/workouts/frog_jumps.gif'// str
 }, {
   id: 4, // int
   name: 'Jumping Jacks', // str
-  gifUrl: 'assets/jumping_jacks.gif'// str
+  gifUrl: 'assets/workouts/jumping_jacks.gif'// str
 }, {
   id: 5, // int
   name: 'Lunges', // str
-  gifUrl: 'assets/lunge.gif'// str
+  gifUrl: 'assets/workouts/lunge.gif'// str
 }, {
   id: 6, // int
   name: 'Lying Hip Raisers', // str
-  gifUrl: 'assets/lying_hip_raise.gif'// str
+  gifUrl: 'assets/workouts/lying_hip_raise.gif'// str
 }, {
   id: 7, // int
   name: 'Mountain Climbers', // str
-  gifUrl: 'assets/mountain_climbers.gif'// str
+  gifUrl: 'assets/workouts/mountain_climbers.gif'// str
 }, {
   id: 8, // int
   name: 'Pile Squats', // str
-  gifUrl: 'assets/pile_squats.gif'// str
+  gifUrl: 'assets/workouts/pile_squats.gif'// str
 }, {
   id: 9, // int
   name: 'Planks', // str
-  gifUrl: 'assets/plank.gif'// str
+  gifUrl: 'assets/workouts/plank.gif'// str
 }, {
   id: 10, // int
   name: 'Push Ups', // str
-  gifUrl: 'assets/push_up.gif'// str
+  gifUrl: 'assets/workouts/push_up.gif'// str
 }, {
   id: 11, // int
   name: 'Side Lunges', // str
-  gifUrl: 'assets/side_lunges.gif'// str
+  gifUrl: 'assets/workouts/side_lunges.gif'// str
 }, {
   id: 12, // int
   name: 'Single Leg Squat', // str
-  gifUrl: 'assets/single_leg_squat.gif'// str
+  gifUrl: 'assets/workouts/single_leg_squat.gif'// str
 }, {
   id: 13, // int
   name: 'Sit Ups', // str
-  gifUrl: 'assets/sit_ups.gif'// str
+  gifUrl: 'assets/workouts/sit_ups.gif'// str
 }, {
   id: 14, // int
   name: 'Double Leg Lifts', // str
-  gifUrl: 'assets/double_leg_lifts.gif'// str
+  gifUrl: 'assets/workouts/double_leg_lifts.gif'// str
 }, {
   id: 15, // int
   name: 'Spinal Balance', // str
-  gifUrl: 'assets/spinal_balance.gif'// str
+  gifUrl: 'assets/workouts/spinal_balance.gif'// str
 }, {
   id: 16, // int
   name: 'Squat Jumps', // str
-  gifUrl: 'assets/squat_jumps.gif'// str
+  gifUrl: 'assets/workouts/squat_jumps.gif'// str
 }, {
   id: 17, // int
   name: 'Squat', // str
-  gifUrl: 'assets/squat.gif'// str
+  gifUrl: 'assets/workouts/squat.gif'// str
 }, {
   id: 18, // int
   name: 'Tuck Jumps', // str
-  gifUrl: 'assets/tuck_jumps.gif'// str
+  gifUrl: 'assets/workouts/tuck_jumps.gif'// str
 }, {
   id: 19, // int
   name: 'Rest', // str
